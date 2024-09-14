e4fc4605572573cd495fc3ad548371b0172c9450
Rahul Suryavanshi <rahul96435@outlook.com>
2024-09-14 18:12:20 +0000

    first commit

---
+++ Swap_number.java
@@ -0,0 +1,17 @@
+        class Test
+{
+    public static void main(String[] args)
+    {
+        int a = 20 ;
+        int b = 10;
+
+        System.out.println(""+a+""+b);
+   
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    System.out.println(""+a+""+b);
+
+    }
+
+}
